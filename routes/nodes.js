d5d313ecb325afe7ed98293f2f869c6bedea927d
GemmaFornons <gemmafornons@GemmaFornonss-MacBook-Pro.local>
2021-11-29 15:41:19 +0100

    test

--- routes/nodes.js
+++ routes/nodes.js
@@ -2,4 +2,18 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models");
 
+router.post("/", async function (req, res) {
+    try {
+       const {situation, media, StoryId} = req.body;
+       const node = await models.Node.create({situation, media, StoryId})
+
+       res.send({id:node.dataValues.id})
+       
+
+    } catch (error) {
+      res.status(500).send(error);
+    }
+
+});
+
 module.exports = router;
