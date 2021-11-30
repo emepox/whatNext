fa7054c53e67da1d354a83ce87e9d2c07f28d934
carolinakakefuku <carolinakakefuku@carolinakakefukus-MacBook-Pro.local>
2021-11-30 21:56:31 +0100

    Home page working with Parallax

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
+import Home from "./components/Home";
 import React from "react";
+import Navbar from './Navbar';
+import Parallax from "./components/Parallax";
+import DraggableList from "./components/Draggable";
 
 import Register from "./components/Register";
 import Login from "./components/Login";
 
 export default function App() {
   return (
-    <div>
+    <div style={{ width: '100%', height: '100%' }}>
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>  
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/parallax" element={<Parallax />} />
+          <Route path="/draggable" element={<DraggableList items={'Lorem ipsum dolor sit'.split(' ')} />} />
         </Routes>
       </BrowserRouter>  
     </div>
