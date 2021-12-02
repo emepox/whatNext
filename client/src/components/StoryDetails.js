53a457cf56e67463604b6c8475d38c589405ec38
MariaVivancos <mariavivancos@MariaVivancoss-MacBook-Pro.local>
2021-12-02 12:26:39 +0100

    fixed small error

--- client/src/components/StoryDetails.js
+++ client/src/components/StoryDetails.js
@@ -110,7 +110,7 @@ export default function StoryDetails() {
                             <button onClick={() => parallax.current.scrollTo(1)} className="bg-purple-500 px-3 py-2 text-white text-base uppercase tracking-wide rounded-full py-2 px-5 hover:bg-purple-700 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ">START WRITING!</button>
                             </div>
                         </form>
-                        }
+                    
                        : <CreateStory postedStory={postedStory}/>
                         }
                     </div>
