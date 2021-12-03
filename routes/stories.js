f04cb862c44eb55db39ff1e688994d001ecfda64
Anna Yakimchuk <yakimchuk.anna.a@gmail.com>
2021-12-03 11:50:56 +0100

    Chanded Story display routing, now pulls from the real db

--- routes/stories.js
+++ routes/stories.js
@@ -1,113 +1,112 @@
 var express = require("express");
 var router = express.Router();
-var models = require( "../models" );
+var models = require("../models");
 
 const userShouldBeLoggedIn = require("./middleware/userShouldBeLoggedIn");
 
-
 // gets story information (for homepage, for example)
 router.get("/", async function (req, res) {
-    try {
-      const stories = await models.Story.findAll(
-        {
-            attributes: ['id', 'name','description','category','reproductions', 'rating', 'media'],
-            where: { isFinished: 0 },
-            include: {model:models.User, attributes:['username', "id"]} 
-        }
-      );
-  
-      res.send(stories);
-      
-    } catch (error) {
-      res.status(500).send(error);
-    }
+  try {
+    const stories = await models.Story.findAll({
+      attributes: [
+        "id",
+        "name",
+        "description",
+        "category",
+        "reproductions",
+        "rating",
+        "media",
+      ],
+      where: { isFinished: 0 },
+      include: { model: models.User, attributes: ["username", "id"] },
+    });
+
+    res.send(stories);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 //gets story with a specific id
 router.get("/:id", async function (req, res) {
-    try {
-        const { id } = req.params
-        const stories = await models.Story.findOne(
-            {
-                where: { id },
-                include: {model:models.User, attributes:['username']} 
-            }
-          );
-      
-          res.send(stories);
-      
-    } catch (error) {
-      res.status(500).send(error);
-    }
+  try {
+    const { id } = req.params;
+    const stories = await models.Story.findOne({
+      where: { id },
+      include: { model: models.User, attributes: ["username"] },
+    });
+
+    res.send(stories);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // gets all nodes from a story
 router.get("/:id/nodes", async function (req, res) {
   try {
-      const { id } = req.params;
-      const nodes = await models.Node.findAll(
-      {
-          attributes: ['id', 'situation'],
-          where: { StoryId: id },
-      }
-    );
+    const { id } = req.params;
+    const nodes = await models.Node.findAll({
+      attributes: ["id", "situation"],
+      where: { StoryId: id },
+    });
 
     res.send(nodes);
-    
   } catch (error) {
     res.status(500).send(error);
   }
 });
 
 // adds new story, returns story.id (TODO:configure first so it gets the id of the first node of the story)
-router.post( "/", userShouldBeLoggedIn, async function ( req, res ) {
-  
+router.post("/", userShouldBeLoggedIn, async function (req, res) {
   const { user_id } = req;
-    try {
-      const {name, description, media, category, isPrivate, isFinished} = req.body;
-       const story = await models.Story.create({ name, description, UserId: user_id, media, category, isPrivate, isFinished})
-       res.send(story.dataValues)
-       
-    } catch (error) {
-      res.status(500).send(error);
-    }
-
+  try {
+    const { name, description, media, category, isPrivate, isFinished } =
+      req.body;
+    const story = await models.Story.create({
+      name,
+      description,
+      UserId: user_id,
+      media,
+      category,
+      isPrivate,
+      isFinished,
+    });
+    res.send(story.dataValues);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 router.put("/:id/first", async function (req, res) {
-  try{
-      const { id } = req.params
-      const {firstId} = req.body
-      await models.Story.update({first:firstId},
-          {
-              where: { id }
-          }
-      );
-  
-  res.send("Story successfully deleted!")
-  
+  try {
+    const { id } = req.params;
+    const { firstId } = req.body;
+    await models.Story.update(
+      { first: firstId },
+      {
+        where: { id },
+      }
+    );
+
+    res.send("Story successfully deleted!");
   } catch (error) {
-  res.status(500).send(error);
+    res.status(500).send(error);
   }
 });
 
 //deletes story by id
 router.delete("/:id", async function (req, res) {
-    try{
-        const { id } = req.params
-        const stories = await models.Story.destroy(
-            {
-                where: { id }
-            }
-        );
-    
-    res.send("Story successfully deleted!")
-    
-    } catch (error) {
+  try {
+    const { id } = req.params;
+    const stories = await models.Story.destroy({
+      where: { id },
+    });
+
+    res.send("Story successfully deleted!");
+  } catch (error) {
     res.status(500).send(error);
-    }
+  }
 });
-
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,47 +3,43 @@ var router = express.Router();
 require("dotenv").config();
 var bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
-var models = require("../models")
-const userShouldBeLoggedIn = require("./middleware/userShouldBeLoggedIn")
-const usernameNotTaken = require("./middleware/usernameNotTaken")
+var models = require("../models");
+const userShouldBeLoggedIn = require("./middleware/userShouldBeLoggedIn");
+const usernameNotTaken = require("./middleware/usernameNotTaken");
 const saltRounds = 10;
 const supersecret = process.env.SUPER_SECRET;
 
-
-
-router.post( "/register", usernameNotTaken, async ( req, res ) => {
+router.post("/register", usernameNotTaken, async (req, res) => {
   const { username, password, email } = req.body;
-  console.log( username, email, password )
+  console.log(username, email, password);
   console.log(supersecret);
   try {
-
     const hash = await bcrypt.hash(password, saltRounds);
 
-    await models.User.create({ username, password: hash, email  });
+    await models.User.create({ username, password: hash, email });
 
     res.send({ message: "Register successful" });
-
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
 });
 
-router.post( "/login", async ( req, res ) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const user = await models.User.findOne( { where: { username } } );
+    const user = await models.User.findOne({ where: { username } });
 
     if (user) {
       const user_id = user.id;
 
-      const correctPassword = await bcrypt.compare( password, user.password );
+      const correctPassword = await bcrypt.compare(password, user.password);
 
-      if ( !correctPassword ) throw new Error( "Incorrect password" );
-      console.log("Llega hasta aquí", user_id)
+      if (!correctPassword) throw new Error("Incorrect password");
+      console.log("Llega hasta aquí", user_id);
 
-      var token = jwt.sign( { user_id }, supersecret );
+      var token = jwt.sign({ user_id }, supersecret);
 
-      console.log("token", token)
+      console.log("token", token);
       res.send({ message: "Login successful, here is your token", token });
     } else {
       throw new Error("User does not exist");
@@ -54,11 +50,11 @@ router.post( "/login", async ( req, res ) => {
 });
 
 router.get("/dashboard", userShouldBeLoggedIn, async (req, res) => {
-  const {user_id} = req
+  const { user_id } = req;
   try {
-    const user = await models.User.findOne({ where: { id:user_id } })
-    res.send(user)
-  } catch(error){
+    const user = await models.User.findOne({ where: { id: user_id } });
+    res.send(user);
+  } catch (error) {
     res.status(400).send({ message: err.message });
   }
 });
@@ -66,16 +62,13 @@ router.get("/dashboard", userShouldBeLoggedIn, async (req, res) => {
 // gets all stories from one user
 router.get("/profile", userShouldBeLoggedIn, async function (req, res) {
   try {
-      const { user_id } = req
-      const stories = await models.Story.findAll(
-          {
-              where: { UserId: user_id },
-              include: {model:models.User, attributes:['username']} 
-          }
-        );
-    
+    const { user_id } = req;
+    const stories = await models.Story.findAll({
+      where: { UserId: user_id },
+      include: { model: models.User, attributes: ["username"] },
+    });
+
     res.send(stories);
-    
   } catch (error) {
     res.status(500).send(error);
   }
