54c5bbfe38f21cf54aa49c1a04c01c646c9ab2c9
MariaVivancos <mariavivancos@MariaVivancoss-MacBook-Pro.local>
2021-12-07 18:25:35 +0100

    Users pending

--- models/user.js
+++ models/user.js
@@ -1,7 +1,17 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const bcrypt = require( "bcrypt" );
+const jwt = require( "jsonwebtoken" );
+const { Model } = require( 'sequelize' );
+const saltRounds = 10;
+
+const hashFunction = (user, options) => {
+  const hash = bcrypt.hash(password, saltRounds);
+  user.password = hash;
+};
+
+
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,15 +23,21 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       User.hasMany(models.Story);
     }
-  };
-  User.init({
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    favourites: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  }
+  User.init(
+    {
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      email: DataTypes.STRING,
+      favourites: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+
+  User.beforeCreate(hashFunction);
+
   return User;
 };
--- seeders/20211203161522-demo-users.js
+++ seeders/20211203161522-demo-users.js
@@ -7,37 +7,28 @@ module.exports = {
     [
       {
         username: "margarita",
-        password:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJpYXQiOjE2MzgzNTc2NDJ9.0_h8w_FScmUDtYHAzOK82sH9iLuio9Vfpdiyhi68RPY",
-        email: "margarita@margarita.margarita",
-        favourites: "1,2,3,4",
+        password: "1234",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         username: "pepe",
-        password:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJpYXQiOjE2MzgzNTc2NDJ9.0_h8w_FScmUDtYHAzOK82sH9iLuio9Vfpdiyhi68RPY",
-        email: "margarita@margarita.margarita",
-        favourites: "1,2,3,4",
+        password: "1234",
+        email: "pepe@pepe.pepe",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         username: "fulano",
-        password:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJpYXQiOjE2MzgzNTc2NDJ9.0_h8w_FScmUDtYHAzOK82sH9iLuio9Vfpdiyhi68RPY",
-        email: "margarita@margarita.margarita",
-        favourites: "1,2,3,4",
+        password: "1234",
+        email: "fulano@fulano.fulano",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         username: "mengano",
-        password:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4LCJpYXQiOjE2MzgzNTc2NDJ9.0_h8w_FScmUDtYHAzOK82sH9iLuio9Vfpdiyhi68RPY",
-        email: "margarita@margarita.margarita",
-        favourites: "1,2,3,4",
+        password: "1234",
+        email: "mengano@mengano.mengano",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
