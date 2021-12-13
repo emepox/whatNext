7f6bfaf64742bdcc7b87abccb4b6a47f5459fe69
carolinakakefuku <carolinakakefuku@carolinakakefukus-MacBook-Pro.local>
2021-12-13 11:42:13 +0100

    styling searchbar

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -34,7 +34,7 @@ export default function Searchbar({
           <div className="flex border-b border-gray-200">
             <button
               onClick={() => requestData()}
-              className="fontAwesome flex items-center h-10 px-2 py-2 -mb-px text-center text-gray-700 bg-transparent border-b-2 border-transparent sm:px-4 -px-1 whitespace-nowrap focus:outline-none hover:border-gray-400 focus:border-purple-500 focus:text-purple-500"
+              className="fontAwesome flex items-center h-10 px-2 py-2 -mb-px text-center text-gray-700 bg-transparent border-b-2 border-transparent sm:px-4 -px-1 whitespace-nowrap focus:outline-none hover:border-gray-400 focus:border-blue-500 focus:text-blue-500"
             >
               <span>&#xf15c;</span>
               <span className="mx-1 text-sm sm:text-base"> My WhatNexts </span>
@@ -42,7 +42,7 @@ export default function Searchbar({
 
             <button
               onClick={() => showFavourites()}
-              className="fontAwesome flex items-center h-10 px-2 py-2 -mb-px text-center text-gray-700 bg-transparent border-b-2 border-transparent sm:px-4 -px-1 whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400 focus:border-purple-500 focus:text-purple-500"
+              className="fontAwesome flex items-center h-10 px-2 py-2 -mb-px text-center text-gray-700 bg-transparent border-b-2 border-transparent sm:px-4 -px-1 whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400 focus:border-blue-500 focus:text-blue-500"
             >
               <span>&#xf004;</span>
               <span className="mx-2 text-sm sm:text-base"> Favourites </span>
