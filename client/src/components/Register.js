85c2c51c262b648aaead93b036a45a300da0ec2a
carolinakakefuku <carolinakakefuku@carolinakakefukus-MacBook-Pro.local>
2021-12-13 11:37:59 +0100

    Error fixed in register page

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -105,7 +105,7 @@ export default function Register() {
             </div>
 
             <div className="flex flex-col mb-6">
-              <label for="email" className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">Password:</label>
+              <label for="email" className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">Email:</label>
               <div className="relative">
                 <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
                   <span>
